10e058dade4926d3ec7bca5d956ba5fd4e305177
Елена Григорьева <elenagrigoreva@MacBook-Pro-Elena.local>
2022-09-13 11:40:45 +0300

    Решение задачи 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n) {
+    Console.Write("m не может быть меньше n в заданной последовательности'");
+    return;
+}
+
+string str = "";
+
+for (int i = m; i <= n; i++) {
+    str += (i - 2) + (i - 1) + " ";
+}
+
+Console.WriteLine(str);
